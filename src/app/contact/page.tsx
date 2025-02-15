de5ab30894f7d0f67fb98931f28bfca4d9f5a36b
PhongNDH <phongndhdev@gmail.com>
2025-02-15 11:36:24 +0700

    first commit

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,30 @@
+import {Label} from "@/components/ui/label";
+import {Input} from "@/components/ui/input";
+
+
+export default function Component() {
+    return (
+        <div className="w-1/2 p-8 mx-auto">
+            <div className="space-y-4">
+                <div className="space-y-2">
+                    <Label>Tên dự án</Label>
+                    <Input placeholder="Coming back with NYC"/>
+                </div>
+                <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
+                    <div className="space-y-2">
+                        <Label>Vai trò</Label>
+                        <Input placeholder="Người mẫu"/>
+                    </div>
+                    <div className="space-y-2">
+                        <Label>Công ty</Label>
+                        <Input placeholder="ABC Company"/>
+                    </div>
+                    <div className="space-y-2">
+                        <Label>Chi tiết dự án</Label>
+                        <Input placeholder="Link"/>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
