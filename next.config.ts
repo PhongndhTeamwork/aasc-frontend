42e7037733c8be21cbf22ce2a60231ba09f632fe
PhongNDH <phongndhdev@gmail.com>
2025-02-15 14:58:10 +0700

    first commit

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-    experimental : {
-        serverActions : {
-            allowedOrigins: ["https://dinhphong.bitrix24.vn"],
-        }
-    }
 };
 
 export default nextConfig;
